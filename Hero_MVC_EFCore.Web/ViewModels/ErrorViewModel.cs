c8116499ccb8efdc202356d5bc853f3bb575444e
Paulo de Tarso <potassio@hotmail.com>
2023-07-27 20:19:25 -0300

    Add project files.

---
+++ Hero_MVC_EFCore.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hero_MVC_EFCore.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
