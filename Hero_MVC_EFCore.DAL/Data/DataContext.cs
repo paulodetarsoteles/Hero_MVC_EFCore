89b2dace283e2bfe113579f020d900457d531dc4
Paulo de Tarso <potassio@hotmail.com>
2023-07-27 20:58:32 -0300

    Criação do contexto, configuração da string de conexão da Program.cs e criação da classe de identidade secreta

---
+++ Hero_MVC_EFCore.DAL/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Hero_MVC_EFCore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hero_MVC_EFCore.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<SecretIdentity> SecretIdentities { get; set; }
+    }
+}
