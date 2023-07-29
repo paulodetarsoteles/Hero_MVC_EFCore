4e10a60c75fcf19563e03870834e9c9e5aba7f07
Paulo de Tarso <potassio@hotmail.com>
2023-07-29 00:33:55 -0300

    Ajustadas as interfaces dos repositórios

--- Hero_MVC_EFCore.DAL/Repositories/Interfaces/IFilmRepository.cs
+++ Hero_MVC_EFCore.DAL/Repositories/Interfaces/IFilmRepository.cs
@@ -4,6 +4,11 @@ namespace Hero_MVC_EFCore.DAL.Repositories.Interfaces
 {
     public interface IFilmRepository
     {
+        IEnumerable<Film> GetAll();
+        Film? GetById(int id);
+        void Insert(Film entity);
+        void Update(Film entity);
+        void Delete(int id);
         List<Hero> GetHeroes();
         bool IsPresent(Hero hero);
     }
--- Hero_MVC_EFCore.DAL/Repositories/Interfaces/IPowerRepository.cs
+++ Hero_MVC_EFCore.DAL/Repositories/Interfaces/IPowerRepository.cs
@@ -4,6 +4,11 @@ namespace Hero_MVC_EFCore.DAL.Repositories.Interfaces
 {
     public interface IPowerRepository
     {
+        IEnumerable<Power> GetAll();
+        Power? GetById(int id);
+        void Insert(Power entity);
+        void Update(Power entity);
+        void Delete(int id);
         Hero GetHero();
     }
 }
